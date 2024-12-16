c24d8f170319d6bfbaa8b9d5c41574391d0348a4
Dennis Hickox <frozzel@me.com>
2024-12-16 12:17:39 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,122 @@
+[ArwinVideo.webm](https://github.com/user-attachments/assets/96ac6a56-6f0e-428d-80c0-6e03ef80d4a2)
+
+---
+
+# Azure 3D Avatar and ChatGPT Integration
+
+[![Github](https://img.shields.io/badge/GitHub-181717.svg?style=for-the-badge&logo=GitHub&logoColor=white)](https://github.com/frozzel/TALKING_AVATAR_BACKEND-MAIN)  [![DIGITALOCEAN](https://img.shields.io/badge/DigitalOcean-0080FF.svg?style=for-the-badge&logo=DigitalOcean&logoColor=white)](https://avatar-ai-app-sbz2z.ondigitalocean.app/)
+
+## Overview
+
+This MERN (MongoDB, Express.js, React.js, Node.js) application integrates an Azure-powered 3D avatar with advanced features such as animated voice, eye movement, and conversational capabilities powered by ChatGPT. The application creates an engaging, lifelike interaction experience, combining modern front-end design with robust back-end functionality.
+
+[![Github license](https://img.shields.io/badge/Client-0275d8.svg)](https://github.com/frozzel/TALKING_AVATAR-MAIN)   [![Github license](https://img.shields.io/badge/Server-0275d8.svg)](https://github.com/frozzel/TALKING_AVATAR_BACKEND-MAIN) 
+
+
+## Features
+
+### 3D Avatar Interaction
+- Voice Animation: The 3D avatar syncs its mouth movements with real-time voice output.
+- Eye Movement: The avatar exhibits realistic eye movements, enhancing engagement.
+- Customizable Look and Feel: Easily configure avatar appearance via Azure's configuration options.
+
+>[!TIP]
+> A ThreeJS-powered virtual human being that uses a set of neat [Azure APIs](https://learn.microsoft.com/en-us/azure/cognitive-services/speech-service/how-to-speech-synthesis-viseme) to do some talking!
+
+### Conversational AI
+- ChatGPT Responses: Harnesses OpenAI's ChatGPT API for natural language understanding and responses.
+- Contextual Conversations: Maintains context for multi-turn dialogue, making interactions feel seamless and human-like.
+
+>[!NOTE]
+> Open Ai Documentation [API DOC](https://platform.openai.com/docs/overview)
+
+### Seamless Integration
+- Real-Time Processing: Combines Azure avatar rendering with ChatGPT processing for smooth, real-time conversations.
+- API-Driven: Uses RESTful APIs for efficient communication between front-end, back-end, and external services.
+
+## Tech Stack
+- MongoDB: Database for managing user data, session histories, and logs.
+- Express.js: Back-end framework to manage server-side logic and API endpoints.
+- React.js: Front-end framework for rendering the 3D avatar and managing user interactions.
+- Node.js: Runtime for handling server-side operations and integrating Azure and OpenAI APIs.
+- Azure 3D Avatar SDK: For rendering and controlling the animated avatar.
+- OpenAI API: For ChatGPT integration to provide conversational AI capabilities.
+
+---
+
+## Setup
+
+1. Clone the repository:
+
+``` console
+git clone <repository-url>
+cd <repository-folder>
+```
+
+2. Install dependencies:
+
+``` console
+npm install
+cd client
+npm install
+cd ..
+```
+
+3. Configure environment variables:
+
+- Create a .env file in the root directory with: env
+React:
+``` .env
+REACT_APP_ALTER_API=
+REACT_APP_ROUTER_BASE=http://localhost:8080/
+REACT_APP_ROUTER_API=http://localhost:8080/api/avatar
+```
+Server:
+``` .env
+AZURE_KEY=
+AZURE_REGION=
+OPENAI_API_KEY=
+```
+4. Start the application:
+Server:
+``` console
+npm run dev
+```
+
+React:
+``` console
+npm run start
+```
+---
+
+## Usage
+- Front-End: Interact with the 3D avatar via the web interface.
+- Back-End: Manage API requests, process avatar animations, and handle conversations.
+- Customizations: Modify avatar settings and conversation flow through admin configurations.
+
+## Future Enhancements
+- Support for multi-language conversations.
+- Advanced avatar gestures and emotional expressions.
+- Integration with additional APIs for expanded functionality.
+
+## Contributing
+
+  Created by: [@frozzel](https://github.com/frozzel)
+  
+  Please contact me with questions at: [@frozzel](mailto:frozzel@me.com)
+
+## License
+
+  [![Github license](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+Feel free to contribute to this project or reach out for support!
+
+---
+
+## Resources 
+You'll also need https://github.com/bornfree/talking_avatar_backend for the text to speech conversion.
+The text to speech and blendShapes converter for https://github.com/bornfree/talking_avatar.
+
+>[!CAUTION]
+> Resources for avatar only no chat gpt integration
---
+++ src/App.js
@@ -0,0 +1,488 @@
+import React, { Suspense, useEffect, useRef, useState, useMemo } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { useGLTF, useTexture, Loader, Environment, useFBX, useAnimations, OrthographicCamera } from '@react-three/drei';
+import { MeshStandardMaterial } from 'three/src/materials/MeshStandardMaterial';
+import { OrbitControls } from '@react-three/drei';
+
+import { LinearEncoding, sRGBEncoding } from 'three/src/constants';
+import { LineBasicMaterial, MeshPhysicalMaterial, Vector2 } from 'three';
+import ReactAudioPlayer from 'react-audio-player';
+
+import createAnimation from './converter';
+import blinkData from './blendDataBlink.json';
+
+import * as THREE from 'three';
+import axios from 'axios';
+const _ = require('lodash');
+
+const host = process.env.REACT_APP_ROUTER_API;
+const host2 = process.env.REACT_APP_ROUTER_BASE;
+
+function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing, intro, message, setIntro }) {
+
+  let gltf = useGLTF(avatar_url);
+  let morphTargetDictionaryBody = null;
+  let morphTargetDictionaryLowerTeeth = null;
+
+  const [ 
+    bodyTexture, 
+    eyesTexture, 
+    teethTexture, 
+    bodySpecularTexture, 
+    bodyRoughnessTexture, 
+    bodyNormalTexture,
+    teethNormalTexture,
+    // teethSpecularTexture,
+    hairTexture,
+    tshirtDiffuseTexture,
+    tshirtNormalTexture,
+    tshirtRoughnessTexture,
+    hairAlphaTexture,
+    hairNormalTexture,
+    hairRoughnessTexture,
+    ] = useTexture([
+    "/images/body.webp",
+    "/images/eyes.webp",
+    "/images/teeth_diffuse.webp",
+    "/images/body_specular.webp",
+    "/images/body_roughness.webp",
+    "/images/body_normal.webp",
+    "/images/teeth_normal.webp",
+    // "/images/teeth_specular.webp",
+    "/images/h_color.webp",
+    "/images/tshirt_diffuse.png",
+    "/images/tshirt_normal.webp",
+    "/images/tshirt_roughness.webp",
+    "/images/h_alpha.webp",
+    "/images/h_normal.webp",
+    "/images/h_roughness.webp",
+  ]);
+
+  _.each([
+    bodyTexture, 
+    eyesTexture, 
+    teethTexture, 
+    teethNormalTexture, 
+    bodySpecularTexture, 
+    bodyRoughnessTexture, 
+    bodyNormalTexture, 
+    tshirtDiffuseTexture, 
+    tshirtNormalTexture, 
+    tshirtRoughnessTexture,
+    hairAlphaTexture,
+    hairNormalTexture,
+    hairRoughnessTexture
+  ], t => {
+    t.encoding = sRGBEncoding;
+    t.flipY = false;
+  });
+
+  bodyNormalTexture.encoding = LinearEncoding;
+  tshirtNormalTexture.encoding = LinearEncoding;
+  teethNormalTexture.encoding = LinearEncoding;
+  hairNormalTexture.encoding = LinearEncoding;
+
+  
+  gltf.scene.traverse(node => {
+
+
+    if(node.type === 'Mesh' || node.type === 'LineSegments' || node.type === 'SkinnedMesh') {
+
+      node.castShadow = true;
+      node.receiveShadow = true;
+      node.frustumCulled = false;
+
+    
+      if (node.name.includes("Body")) {
+
+        node.castShadow = true;
+        node.receiveShadow = true;
+
+        node.material = new MeshPhysicalMaterial();
+        node.material.map = bodyTexture;
+        // node.material.shininess = 60;
+        node.material.roughness = 1.7;
+
+        // node.material.specularMap = bodySpecularTexture;
+        node.material.roughnessMap = bodyRoughnessTexture;
+        node.material.normalMap = bodyNormalTexture;
+        node.material.normalScale = new Vector2(0.6, 0.6);
+
+        morphTargetDictionaryBody = node.morphTargetDictionary;
+
+        node.material.envMapIntensity = 0.8;
+        // node.material.visible = false;
+
+      }
+
+      if (node.name.includes("Eyes")) {
+        node.material = new MeshStandardMaterial();
+        node.material.map = eyesTexture;
+        // node.material.shininess = 100;
+        node.material.roughness = 0.1;
+        node.material.envMapIntensity = 0.5;
+
+
+      }
+
+      if (node.name.includes("Brows")) {
+        node.material = new LineBasicMaterial({color: 0x000000});
+        node.material.linewidth = 1;
+        node.material.opacity = 0.5;
+        node.material.transparent = true;
+        node.visible = false;
+      }
+
+      if (node.name.includes("Teeth")) {
+
+        node.receiveShadow = true;
+        node.castShadow = true;
+        node.material = new MeshStandardMaterial();
+        node.material.roughness = 0.1;
+        node.material.map = teethTexture;
+        node.material.normalMap = teethNormalTexture;
+
+        node.material.envMapIntensity = 0.7;
+
+
+      }
+
+      if (node.name.includes("Hair")) {
+        node.material = new MeshStandardMaterial();
+        node.material.map = hairTexture;
+        node.material.alphaMap = hairAlphaTexture;
+        node.material.normalMap = hairNormalTexture;
+        node.material.roughnessMap = hairRoughnessTexture;
+        
+        node.material.transparent = true;
+        node.material.depthWrite = false;
+        node.material.side = 2;
+        node.material.color.setHex(0x000000);
+        
+        node.material.envMapIntensity = 0.3;
+
+      
+      }
+
+      if (node.name.includes("TSHIRT")) {
+        node.material = new MeshStandardMaterial();
+
+        node.material.map = tshirtDiffuseTexture;
+        node.material.roughnessMap = tshirtRoughnessTexture;
+        node.material.normalMap = tshirtNormalTexture;
+        node.material.color.setHex(0xffffff);
+
+        node.material.envMapIntensity = 0.5;
+
+
+      }
+
+      if (node.name.includes("TeethLower")) {
+        morphTargetDictionaryLowerTeeth = node.morphTargetDictionary;
+      }
+
+    }
+
+  });
+
+  const [clips, setClips] = useState([]);
+  const mixer = useMemo(() => new THREE.AnimationMixer(gltf.scene), []);
+
+  useEffect(() => {
+
+    if (speak === false)
+      return;
+
+    makeSpeech(text)
+    .then( response => {
+
+      let {blendData, filename}= response.data;
+
+      let newClips = [ 
+        createAnimation(blendData, morphTargetDictionaryBody, 'HG_Body'), 
+        createAnimation(blendData, morphTargetDictionaryLowerTeeth, 'HG_TeethLower') ];
+
+      filename = host2 + filename;
+        
+      setClips(newClips);
+      setAudioSource(filename);
+
+    })
+    .catch(err => {
+      console.error(err, "Error in making speech");
+      setSpeak(false);
+
+    })
+
+  }, [speak]);
+
+  useEffect(() => {
+
+    if (intro === false)
+      return;
+
+    makeSpeech2(message)
+    .then( response => {
+
+      let {blendData, filename}= response.data;
+
+      let newClips = [ 
+        createAnimation(blendData, morphTargetDictionaryBody, 'HG_Body'), 
+        createAnimation(blendData, morphTargetDictionaryLowerTeeth, 'HG_TeethLower') ];
+
+      filename = host2 + filename;
+        
+      setClips(newClips);
+      setAudioSource(filename);
+      setIntro(false);
+
+    })
+    .catch(err => {
+      console.error(err, "Error in making speech");
+      setIntro(false);
+      setSpeak(false);
+
+    })
+
+  }, [speak]);
+
+  let idleFbx = useFBX('/idle.fbx');
+  let { clips: idleClips } = useAnimations(idleFbx.animations);
+
+  idleClips[0].tracks = _.filter(idleClips[0].tracks, track => {
+    return track.name.includes("Head") || track.name.includes("Neck") || track.name.includes("Spine2");
+  });
+
+  idleClips[0].tracks = _.map(idleClips[0].tracks, track => {
+
+    if (track.name.includes("Head")) {
+      track.name = "head.quaternion";
+    }
+
+    if (track.name.includes("Neck")) {
+      track.name = "neck.quaternion";
+    }
+
+    if (track.name.includes("Spine")) {
+      track.name = "spine2.quaternion";
+    }
+
+    return track;
+
+  });
+
+  useEffect(() => {
+
+    let idleClipAction = mixer.clipAction(idleClips[0]);
+    idleClipAction.play();
+
+    let blinkClip = createAnimation(blinkData, morphTargetDictionaryBody, 'HG_Body');
+    let blinkAction = mixer.clipAction(blinkClip);
+    blinkAction.play();
+
+
+  }, []);
+
+  // Play animation clips when available
+  useEffect(() => {
+
+    if (playing === false)
+      return;
+    
+    _.each(clips, clip => {
+        let clipAction = mixer.clipAction(clip);
+        clipAction.setLoop(THREE.LoopOnce);
+        clipAction.play();
+
+    });
+
+  }, [playing]);
+
+  
+  useFrame((state, delta) => {
+    mixer.update(delta);
+  });
+
+
+  return (
+    <group name="avatar">
+      <primitive object={gltf.scene} dispose={null} />
+    </group>
+  );
+}
+
+
+function makeSpeech(text) {
+  return axios.post(host + '/talk', { text });
+}
+
+function makeSpeech2(text) {
+  return axios.post(host + '/talk2', { text });
+}
+
+const STYLES = {
+  area: {position: 'absolute', bottom:'10px', left: '10px', zIndex: 500, display: 'flex', flexDirection: 'column'},
+  text: {margin: '0px', width:'300px', padding: '5px', background: 'none', color: '#ffffff', fontSize: '1.2em', border: 'none'},
+  question: {margin: '0px', width:'300px', padding: '5px', background: 'none', color: '#DC9BD4', fontSize: '1.2em', border: 'none'},
+  speak: {padding: '10px', marginTop: '5px', display: 'block', color: '#FFFFFF', background: '#222222', border: 'None', maxWidth: '80px'},
+  area2: {position: 'absolute', top:'5px', right: '15px', zIndex: 500},
+  label: {color: '#777777', fontSize:'0.8em'}
+}
+
+function App() {
+  const [message, setMessage] = useState("My name is Arwen. I'm a AI virtual assistant who can help you with your questions. Click speak to start a conversation.");
+  const audioPlayer = useRef();
+  const [intro, setIntro] = useState(true);
+  const [speak, setSpeak] = useState(false);
+  const [text, setText] = useState("👇👇👇👇");
+  const [audioSource, setAudioSource] = useState(null);
+  const [playing, setPlaying] = useState(false);
+
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+
+  const handleListen = async () => {
+    setMessage('Listening...');
+    // console.log(message)
+  
+    var quest = null
+  
+    if (!recognition.running) {
+      recognition.start();
+      recognition.onstart = () => {
+        setMessage('Voice recognition started. Speak into the microphone.');
+      };
+      recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        setMessage('Voice recognition result:', transcript);
+        quest = transcript;
+      };
+      recognition.onend =  () => {
+        setMessage('Voice recognition ended.');
+        
+        //  setQuestion(quest)
+        
+        // fetchChatGpt(quest)
+
+        setText(quest);
+        setSpeak(true);
+  
+      };
+      
+      } else {
+        recognition.stop();
+        setMessage('Voice recognition stopped. Click on the Microphone logo to start again.');
+    }
+    
+  };
+
+  // End of play
+  function playerEnded(e) {
+    setAudioSource(null);
+    setSpeak(false);
+    setPlaying(false);
+  }
+
+  // Player is read
+  function playerReady(e) {
+    audioPlayer.current.audioEl.current.play();
+    setPlaying(true);
+
+  }  
+
+  return (
+    <div className="full">
+      <div style={STYLES.area}>
+        <textarea rows={4} type="text" style={STYLES.text} value={message} onChange={(e) => setText(e.target.value.substring(0, 200))} />
+        <textarea rows={4} type="text" style={STYLES.question} value={text} onChange={(e) => setText(e.target.value.substring(0, 200))} />
+        {/* <button onClick={() => setSpeak(true)} style={STYLES.speak}> { speak? 'Running...': 'Speak' }</button> */}
+        <button onClick={() => handleListen()} style={STYLES.speak}> { speak? 'Running...': 'Speak' }</button>
+
+
+      </div>
+
+      <ReactAudioPlayer
+        src={audioSource}
+        ref={audioPlayer}
+        onEnded={playerEnded}
+        onCanPlayThrough={playerReady}
+        
+      />
+      
+      {/* <Stats /> */}
+    <Canvas dpr={2} onCreated={(ctx) => {
+        ctx.gl.physicallyCorrectLights = true;
+      }}>
+
+      <OrthographicCamera 
+      makeDefault
+      zoom={1800}
+      position={[0, 1.65, 1]}
+      />
+
+      <OrbitControls
+        target={[0, 1.65, 0]}
+      />
+
+      <Suspense fallback={null}>
+        <Environment background={false} files="/images/photo_studio_loft_hall_1k.hdr" />
+      </Suspense>
+
+      <Suspense fallback={null}>
+        <Bg />
+      </Suspense>
+
+      <Suspense fallback={null}>
+
+
+
+          <Avatar 
+            avatar_url="/model.glb" 
+            speak={speak} 
+            setSpeak={setSpeak}
+            text={text}
+            setAudioSource={setAudioSource}
+            playing={playing}
+            intro={intro}
+            message={message}
+            setIntro={setIntro}
+            />
+
+      
+      </Suspense>
+
+  
+
+  </Canvas>
+  <Loader dataInterpolation={(p) => `Loading... please wait`}  />
+  </div>
+  )
+}
+
+function Bg() {
+  const {scene} =  useThree();
+  const texture = useTexture('/images/sky.jpg');
+
+  scene.background = texture; 
+
+  return(<>
+    {/* <mesh position={[0, 1.5, -2]} scale={[0.8, 0.8, 0.8]}>
+      <planeBufferGeometry />
+      <meshBasicMaterial map={texture} side={THREE.DoubleSide}/>
+
+    </mesh> */}
+    {/* <mesh
+    visible
+    userData={{ hello: 'world' }}
+    position={new THREE.Vector3(0, 1.5, -3)}
+    rotation={new THREE.Euler(Math.PI / 2, 0, 0)}
+    geometry={new THREE.SphereGeometry(4, 16, 16)}
+    // material={new THREE.MeshBasicMaterial({ color: new THREE.Color('hotpink'), transparent: true })}
+    material={new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide })}
+  /> */}
+    
+  </>)
+
+}
+
+export default App;
